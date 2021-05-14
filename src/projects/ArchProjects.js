d95353b87733b18d79865fb54ad862efe4e51022
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2021-05-14 22:56:27 +0200

    feat: :sparkles: Project images added & projects updated

--- src/projects/ArchProjects.js
+++ src/projects/ArchProjects.js
@@ -1,34 +1,43 @@
 import React from "react";
 
 import CardLarge from "../components/CardLarge";
+import CarouselProjectOne from "../projects/CarouselProjectOne";
+import CarouselProjectTwo from "../projects/CarouselProjectTwo";
+import CarouselProjectThree from "../projects/CarouselProjectThree";
 import YumDark from "../images/YumDark.png";
 import YumLight from "../images/YumLight.png";
 import TswaluGroup from "../images/TswaluGroup.png";
-import EuphorbiaCover from "../images/EuphorbiaCover.png";
+import MashatuGroup from "../images/MashatuGroup.png";
 
 const ArchProjects = ({ darkTheme }) => {
   return (
     <div>
       <CardLarge
+        darkTheme={darkTheme}
         image={darkTheme ? YumDark : YumLight}
         number={"01"}
         title={"YUM! Offices"}
         subtitle={"Office fit-out for new KFC headquarters"}
-        buttonText={"View project"}
+        buttonText={"View gallery"}
+        project={CarouselProjectOne}
       />
       <CardLarge
+        darkTheme={darkTheme}
         image={TswaluGroup}
         number={"02"}
         title={"Tswalu"}
         subtitle={"Kalahari game lodge refurbishment"}
-        buttonText={"View project"}
+        buttonText={"View gallery"}
+        project={CarouselProjectTwo}
       />
       <CardLarge
-        image={EuphorbiaCover}
+        darkTheme={darkTheme}
+        image={MashatuGroup}
         number={"03"}
-        title={"Euphorbia"}
+        title={"Euphorbia Camp"}
         subtitle={"New private game lodge in Botswana"}
-        buttonText={"View project"}
+        buttonText={"View gallery"}
+        project={CarouselProjectThree}
       />
     </div>
   );
